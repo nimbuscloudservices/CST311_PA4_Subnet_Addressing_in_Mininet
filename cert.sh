bcdee1d2c822b63b554b5c01392ec1106c173165
Saul M <smendozaloera@csumb.edu>
2022-06-15 22:06:46 -0700

    doesnt ask to enter password for keys anymore

--- cert.sh
+++ cert.sh
@@ -5,8 +5,8 @@ sudo mkdir -p certs
 sudo mkdir -p keys
 
 # Gernerate CA private key and CA certificate
-sudo openssl genrsa -aes256 -out cakey.pem 2048
-sudo openssl req -x509 -new -nodes -key cakey.pem -sha256 -days 1825 -out cacert.pem -subj "/C=US/ST=California/L=Oakland/O=Nimbus Cloud Services/OU=IT/CN=ca.pa4.test"
+sudo openssl genrsa -aes256 -out cakey.pem -passout pass:NCSPA4 2048
+sudo openssl req -x509 -new -nodes -key cakey.pem -sha256 -days 1825 -out cacert.pem -passin pass:NCSPA4 -subj "/C=US/ST=California/L=Oakland/O=Nimbus Cloud Services/OU=IT/CN=ca.pa4.test"
 
 #Display CA Certificate
 openssl x509 -text -noout -in cacert.pem
@@ -16,11 +16,11 @@ sudo cp cacert.pem /usr/local/share/ca-certificates/cacert.crt
 sudo update-ca-certificates
 
 #Gernerate Web Server's private key and Certificate Signing Request (CSR)
-sudo openssl genrsa -out webserver-key.pem 2048
+sudo openssl genrsa -out webserver-key.pem -passout pass:NCSPA4 2048
 sudo openssl req -nodes -new -config /etc/ssl/openssl.cnf -key webserver-key.pem -out webserver.csr -subj "/C=US/ST=California/L=Oakland/O=Nimbus Cloud Services/OU=IT/CN=www.nimbuscloudservices.test"
 
 #Signed certificate for web server
-sudo openssl x509 -req -days 365 -in webserver.csr -CA cacert.pem -CAkey cakey.pem -CAcreateserial -out webserver-cert.pem
+sudo openssl x509 -req -days 365 -in webserver.csr -CA cacert.pem -CAkey cakey.pem -CAcreateserial -out webserver-cert.pem -passin pass:NCSPA4
 
 #Display certificate for web server
 openssl x509 -text -noout -in webserver-cert.pem
