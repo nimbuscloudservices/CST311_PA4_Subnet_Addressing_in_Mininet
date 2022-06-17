e92ace3d4eba95f996a0197e70ab4d1372d8b9fc
Saul M <smendozaloera@csumb.edu>
2022-06-16 21:27:44 -0700

    maketerms for h1 h2 h3 h4 (h1 not working)

--- legacy_network_given.py
+++ legacy_network_given.py
@@ -21,52 +21,58 @@ __status__ = "Prototype"
 __date__ = "June 2nd, 2022"
 
 def myNetwork():
-    net = Mininet(topo=None,
-                  build=False,
-                  ipBase='10.0.0.0/24')
+    net = Mininet( topo=None,
+                   build=False,
+                   ipBase='10.0.10.0/24')
 
-    info('*** Adding controller\n')
-    c0 = net.addController(name='c0',
-                           controller=Controller,
-                           protocol='tcp',
-                           port=6633)
+    info( '*** Adding controller\n' )
+    c0=net.addController(name='c0',
+                         controller=Controller,
+                         protocol='tcp',
+                         port=6633)
 
-    info('*** Add switches\n')
-    # [Changelog] moved s2 and s1 above r5
+    info( '*** Add switches\n')
+    # moved s2 and s1 above r5
     s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
     s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
 
-    # [Changelog] added IP to r5
-    r5 = net.addHost('r5', cls=Node, ip='10.0.2.1/24')
+    # added IP to r5
+    # added static route to r4 from r5
+    r5 = net.addHost('r5', cls=Node, ip='10.0.20.1/24')
     r5.cmd('sysctl -w net.ipv4.ip_forward=1')
 
-    # [Changelog] added IP to r4
-    r4 = net.addHost('r4', cls=Node, ip='192.168.1.2/30')
+    # added IP to r4
+    # added static routes to r3 and r5 from r4
+    r4 = net.addHost('r4', cls=Node, ip='192.168.30.1/30')
     r4.cmd('sysctl -w net.ipv4.ip_forward=1')
 
-    # [Changelog] added IP to r3
-    r3 = net.addHost('r3', cls=Node, ip='10.0.1.1/24')
+    # added IP to r3
+    # added static route to r4 from r3
+    r3 = net.addHost('r3', cls=Node, ip='10.0.30.1/24')
     r3.cmd('sysctl -w net.ipv4.ip_forward=1')
+    r3.cmd("ip route add 192.168.60.1/30 via 192.168.30.1 dev r3-eth1")
+    r3.cmd("ip route add 192.168.60.2/30 via 192.168.30.1 dev r3-eth1")
 
-    info('*** Add hosts\n')
 
-    # [Changelog] added IP to h1 and the default route of r3
-    h1 = net.addHost('h1', cls=Host, ip='10.0.1.50/24',
-                     defaultRoute='via 10.0.1.1')
-    
-    # [Changelog] added IP to h2 and the default route of r3
-    h2 = net.addHost('h2', cls=Host, ip='10.0.1.100/24',
-                     defaultRoute='via 10.0.1.1')
-    
-    # [Changelog] added IP to h3 and the default route of r5
-    h3 = net.addHost('h3', cls=Host, ip='10.0.2.100/24',
-                     defaultRoute='via 10.0.2.1')
-    
-    # [Changelog] added IP to h4 and the default route of r5
-    h4 = net.addHost('h4', cls=Host, ip='10.0.2.50/24',
-                     defaultRoute='via 10.0.2.1')
+    info( '*** Add hosts\n')
+    # added IP to h1 and the default route of r3
+    h1 = net.addHost( 'h1', cls=Host, ip='10.0.30.2/24',
+                      defaultRoute='10.0.30.1')
 
-    info('*** Add links\n')
+    # added IP to h2 and the default route of r3
+    h2 = net.addHost( 'h2', cls=Host, ip='10.0.30.3/24',
+                      defaultRoute='10.0.30.1')
+
+    # added IP to h3 and the default route of r5
+    h3 = net.addHost( 'h3', cls=Host, ip='10.0.20.2/24',
+                      defaultRoute='10.0.20.1')
+
+    # added IP to h4 and the default route of r5
+    h4 = net.addHost( 'h4', cls=Host, ip='10.0.20.3/24',
+                      defaultRoute='10.0.20.1')
+
+
+    info( '*** Add links\n')
     net.addLink(h1, s1)
     net.addLink(h2, s1)
     net.addLink(h3, s2)
@@ -74,49 +80,47 @@ def myNetwork():
     net.addLink(s2, r5)
     net.addLink(s1, r3)
 
-    # [Changelog] added IP address to r3-eth1
-    net.addLink(r3, r4, intfName1='r3-eth1',
-                params1={'ip': '192.168.1.1/30'})
+    # added intfName1 of r3
+    # added intfName2 of r4
+    net.addLink( r3,
+                 r4,
+                 intfName1='r3-eth1',
+                 intfName2='r4-eth0',
+                 params1={ 'ip' : '192.168.30.2/30' },
+                 params2={ 'ip' : '192.168.30.1/30' } )
 
-    # [Changelog] added IP address to r4-eth1 and r5-eth1
-    net.addLink(r4, r5, intfName1='r4-eth1',
-                params1={'ip': '192.168.1.5/30'},
+    # added intfName1 of r4
+    # added intfName2 of r5
+    net.addLink(r4,
+                r5,
+                intfName1='r4-eth1',
                 intfName2='r5-eth1',
-                params2={'ip': '192.168.1.6/30'})
+                params1={ 'ip' : '192.168.60.1/30' },
+                params2={ 'ip' : '192.168.60.2/30' } )
 
-    info('*** Starting network\n')
+    info( '*** Starting network\n')
     net.build()
-
-    info('*** Starting controllers\n')
+    info( '*** Starting controllers\n')
     for controller in net.controllers:
         controller.start()
 
-    info('*** Starting switches\n')
+    info( '*** Starting switches\n')
     net.get('s2').start([c0])
     net.get('s1').start([c0])
 
-    info('*** Post configure switches and hosts\n')
+    info( '*** Post configure switches and hosts\n')
 
-    info('*** Adding static routes for r3, r4, r5\n')
-
-    # [Changelog] added route to 10.0.2.0/24 and 192.168.1.4/30 
-    r3.cmd('ip route add 10.0.2.0/24 via 192.168.1.2 dev r3-eth1')
-    r3.cmd('ip route add 192.168.1.4/30 via 192.168.1.2 dev r3-eth1')
-
-    # [Changelog] added route to 10.0.2.0/24 and 10.0.1.0/24 
-    r4.cmd('ip route add 10.0.2.0/24 via 192.168.1.6 dev r4-eth1')
-    r4.cmd('ip route add 10.0.1.0/24 via 192.168.1.1 dev r4-eth0')
-
-    # [Changelog] added route to 10.0.1.0/24 and 192.168.1.0/30
-    r5.cmd('ip route add 10.0.1.0/24 via 192.168.1.5 dev r5-eth1')
-    r5.cmd('ip route add 192.168.1.0/30 via 192.168.1.5 dev r5-eth1')
-
+    # start h1, h2, h3, h4 terminals
+    chatServer=makeTerm(h4, 'Chat Server', 'xterm', None, 'python3 chat_server.py')
+    clientXChat=makeTerm(h1, 'Client Chat', 'xterm', None, 'python3 chat_client.py')
+    clientYChat=makeTerm(h3, 'Client Chat', 'xterm', None, 'python3 chat_client.py')
+    tslwebServer=makeTerm(h2, 'TLS-enabled Simple Web Server', 'xterm', None, 'python3 web_server.py')
+    
     # [Changelog] added cmd to start webserver on
     CLI(net)
-    net.makeTerm
+    net.makeTerm(h)
     net.stop()
 
-
 if __name__ == '__main__':
-    setLogLevel('info')
+    setLogLevel( 'info' )
     myNetwork()
